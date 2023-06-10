711021dea1a2d442dd4d84eb667ec4ae7c967609
Alperen Erdem <alpernerdm@gmail.com>
2023-06-10 13:07:51 +0300

    Login hata çözüldü

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -9,6 +9,7 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class CariPanelController : Controller
     {
         // GET: CariPanel
+        [Authorize]
         public ActionResult Index()
         {
             return View();
